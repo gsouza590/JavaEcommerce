c9584235afccf6cca66a7e272b2050e3f3e3959a
gsouza590 <gsouza590@gmail.com>
2023-10-23 14:23:36 -0300

    Creating Repository and Configura applicationProperties

--- Admin/src/main/java/com/gabriel/Admin/AdminApplication.java
+++ Admin/src/main/java/com/gabriel/Admin/AdminApplication.java
@@ -2,8 +2,12 @@ package com.gabriel.Admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.gabriel.Backend.*", "com.gabriel.Admin.*"})
+@EnableJpaRepositories(value = "com.gabriel.Backend.repository")
+@EntityScan(value = "com.gabriel.Backend.model")
 public class AdminApplication {
 
 	public static void main(String[] args) {
---
+++ Backend/src/main/java/com/gabriel/Backend/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.Backend.repository;
+
+import com.gabriel.Backend.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository  extends JpaRepository<Admin, Long> {
+}
