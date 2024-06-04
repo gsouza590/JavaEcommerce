be7c27450f61fa506fef825284f077cdecfeeb4a
gsouza590 <gsouza590@gmail.com>
2024-06-04 17:24:20 -0300

    update readability and immutability

--- Customer/src/main/java/com/gabriel/Customer/config/CustomerConfiguration.java
+++ Customer/src/main/java/com/gabriel/Customer/config/CustomerConfiguration.java
@@ -1,6 +1,5 @@
 package com.gabriel.Customer.config;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +16,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class CustomerConfiguration {
+
     @Bean
     public CustomerServiceConfig userDetailsService() {
         return new CustomerServiceConfig();
@@ -28,43 +28,44 @@ public class CustomerConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder
-                = http.getSharedObject(AuthenticationManagerBuilder.class);
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/*", "/product-detail/**", "/cancel-order/**", "/find-products/**").permitAll()
+                        .requestMatchers("/shop/**").hasAuthority("CUSTOMER")
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/do-login")
+                        .defaultSuccessUrl("/index", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                )
+                .httpBasic(httpBasic -> httpBasic.disable()); // if not using basic auth
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
 
         authenticationManagerBuilder
                 .userDetailsService(userDetailsService())
                 .passwordEncoder(passwordEncoder());
 
-        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
-
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(author ->
-                        author.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                .requestMatchers("/*", "/product-detail/**","/cancel-order/**","/find-products/**").permitAll()
-                                .requestMatchers("/shop/**").hasAuthority("CUSTOMER")
-                )
-                .formLogin(login ->
-                        login.loginPage("/login")
-                                .loginProcessingUrl("/do-login")
-                                .defaultSuccessUrl("/index", true)
-                                .permitAll()
-                )
-                .logout(logout ->
-                        logout.invalidateHttpSession(true)
-                                .clearAuthentication(true)
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                                .logoutSuccessUrl("/login?logout")
-                                .permitAll()
-                )
-                .authenticationManager(authenticationManager)
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                )
-        ;
-        return http.build();
+        return authenticationManagerBuilder.build();
     }
-
 }
-
