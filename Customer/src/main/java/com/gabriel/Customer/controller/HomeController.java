5471466b9262267383d82f6096b32ec07ca444ad
gsouza590 <gsouza590@gmail.com>
2024-06-05 14:42:42 -0300

    update: separated method improve clarity

--- Customer/src/main/java/com/gabriel/Customer/controller/HomeController.java
+++ Customer/src/main/java/com/gabriel/Customer/controller/HomeController.java
@@ -5,7 +5,6 @@ import com.gabriel.Backend.model.ShoppingCart;
 import com.gabriel.Backend.service.CustomerService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,25 +14,19 @@ import java.security.Principal;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    @Autowired
-    private CustomerService customerService;
 
+    private final CustomerService customerService;
 
-    @GetMapping(value = {"/","index"})
-    public String home(Model model, Principal principal, HttpSession session){
-
+    @GetMapping(value = {"/", "index"})
+    public String home(Model model, Principal principal, HttpSession session) {
         model.addAttribute("title", "Home");
-        model.addAttribute("page","Home");
-        if(principal!=null){
-            Customer customer = customerService.findByUsername(principal.getName());
-            session.setAttribute("username", customer.getFirstName()+ " " + customer.getLastName());
-            ShoppingCart shoppingCart = customer.getShoppingCart();
-            if(shoppingCart!= null){
-                session.setAttribute("totalItems" ,shoppingCart.getTotalItems());
-            }
+        model.addAttribute("page", "Home");
+        if (principal != null) {
+            setupSessionForAuthenticatedUser(session, principal);
         }
         return "home";
     }
+
     @GetMapping("/contact")
     public String contact(Model model) {
         model.addAttribute("title", "Contato");
@@ -41,5 +34,12 @@ public class HomeController {
         return "contact";
     }
 
+    private void setupSessionForAuthenticatedUser(HttpSession session, Principal principal) {
+        Customer customer = customerService.findByUsername(principal.getName());
+        session.setAttribute("username", customer.getFirstName() + " " + customer.getLastName());
+        ShoppingCart shoppingCart = customer.getShoppingCart();
+        if (shoppingCart != null) {
+            session.setAttribute("totalItems", shoppingCart.getTotalItems());
+        }
     }
-
+}
