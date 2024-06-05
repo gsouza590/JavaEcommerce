6ec34db752cec92d37f27862a3f0385cdbea8844
gsouza590 <gsouza590@gmail.com>
2024-06-05 14:43:58 -0300

    update constructor injection improve consistency

--- Customer/src/main/java/com/gabriel/Customer/controller/OrderController.java
+++ Customer/src/main/java/com/gabriel/Customer/controller/OrderController.java
@@ -7,94 +7,80 @@ import com.gabriel.Backend.service.CountryService;
 import com.gabriel.Backend.service.CustomerService;
 import com.gabriel.Backend.service.OrderService;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class OrderController {
 
-    @Autowired
-    private CustomerService customerService;
-    @Autowired
-    private CityService cityService;
-    @Autowired
-    private CountryService countryService;
-
-    @Autowired
-    private OrderService orderService;
+    private final CustomerService customerService;
+    private final CityService cityService;
+    private final CountryService countryService;
+    private final OrderService orderService;
 
     @GetMapping("/order")
     public String getOrders(Model model, Principal principal) {
         if (principal == null) {
             return "redirect:/login";
-        } else {
-            Customer customer = customerService.findByUsername(principal.getName());
-            List<Order> orderList = customer.getOrders();
-            model.addAttribute("orders", orderList);
-            model.addAttribute("title", "Pedido");
-            model.addAttribute("page", "Pedido");
-            return "order";
         }
+        Customer customer = customerService.findByUsername(principal.getName());
+        List<Order> orderList = customer.getOrders();
+        model.addAttribute("orders", orderList);
+        model.addAttribute("title", "Pedido");
+        model.addAttribute("page", "Pedido");
+        return "order";
     }
 
-    @RequestMapping(value = "/add-order", method = {RequestMethod.POST})
+    @PostMapping("/add-order")
     @Transactional
-    public String createOrder(Principal principal,
-                              Model model,
-                              HttpSession session) {
+    public String createOrder(Principal principal, Model model, HttpSession session) {
         if (principal == null) {
             return "redirect:/login";
-        } else {
-            Customer customer = customerService.findByUsername(principal.getName());
-            ShoppingCart cart = customer.getShoppingCart();
-            Order order = orderService.save(cart);
-            session.removeAttribute("totalItems");
-            model.addAttribute("order", order);
-            model.addAttribute("title", "Detalhes Pedido");
-            model.addAttribute("page", "Detalhes Pedido");
-            model.addAttribute("success", "Pedido adicionado com Sucesso!!");
-            return "order-detail";
         }
+        Customer customer = customerService.findByUsername(principal.getName());
+        ShoppingCart cart = customer.getShoppingCart();
+        Order order = orderService.save(cart);
+        session.removeAttribute("totalItems");
+        model.addAttribute("order", order);
+        model.addAttribute("title", "Detalhes Pedido");
+        model.addAttribute("page", "Detalhes Pedido");
+        model.addAttribute("success", "Pedido adicionado com Sucesso!!");
+        return "order-detail";
     }
 
     @GetMapping("/check-out")
     public String checkOut(Principal principal, Model model) {
         if (principal == null) {
             return "redirect:/login";
-        } else {
-            CustomerDto customer = customerService.getCustomer(principal.getName());
-            if (customer.getAddress() == null || customer.getCity() == null || customer.getPhoneNumber() == null) {
-                model.addAttribute("information", "Necessário atualizar os dados pessoais antes de continuar!");
-                List<Country> countryList = countryService.findAll();
-                List<City> cities = cityService.findAll();
-                model.addAttribute("customer", customer);
-                model.addAttribute("cities", cities);
-                model.addAttribute("countries", countryList);
-                model.addAttribute("title", "Perfil");
-                model.addAttribute("page", "Perfil");
-                return "customer-information";
-            } else {
-                ShoppingCart cart = customerService.findByUsername(principal.getName()).getShoppingCart();
-                model.addAttribute("customer", customer);
-                model.addAttribute("title", "Check-Out");
-                model.addAttribute("page", "Check-Out");
-                model.addAttribute("shoppingCart", cart);
-                model.addAttribute("grandTotal", cart.getTotalItems());
-                return "checkout";
-            }
         }
+        CustomerDto customer = customerService.getCustomer(principal.getName());
+        if (customer.getAddress() == null || customer.getCity() == null || customer.getPhoneNumber() == null) {
+            model.addAttribute("information", "Necessário atualizar os dados pessoais antes de continuar!");
+            List<Country> countryList = countryService.findAll();
+            List<City> cities = cityService.findAll();
+            model.addAttribute("customer", customer);
+            model.addAttribute("cities", cities);
+            model.addAttribute("countries", countryList);
+            model.addAttribute("title", "Perfil");
+            model.addAttribute("page", "Perfil");
+            return "customer-information";
+        }
+        ShoppingCart cart = customerService.findByUsername(principal.getName()).getShoppingCart();
+        model.addAttribute("customer", customer);
+        model.addAttribute("title", "Check-Out");
+        model.addAttribute("page", "Check-Out");
+        model.addAttribute("shoppingCart", cart);
+        model.addAttribute("grandTotal", cart.getTotalItems());
+        return "checkout";
     }
-
 
     @RequestMapping(value = "/cancel-order/{id}", method = {RequestMethod.PUT, RequestMethod.GET})
     public String cancelOrder(@PathVariable Long id, RedirectAttributes attributes) {
@@ -102,6 +88,4 @@ public class OrderController {
         attributes.addFlashAttribute("success", "Pedido cancelado com sucesso!");
         return "redirect:/order";
     }
-
-
 }
